44ceef8d8f2ff10c760501b3b09c25657a5ce9d8
chanoga <chanogab@gmail.com>
2021-03-08 12:50:52 -0800

    more explanation of class

--- OOP/Introduction.cpp
+++ OOP/Introduction.cpp
@@ -188,23 +188,170 @@
 
     Using this Keyword:
     ------------------
-   comming soon
+   This keyword is a pointer to the object of a class,it is 
+   used to refer the object.
+
+   example:
+
+   class D{
+       public D(string name){
+           this->name = name; here name is the argument of the string as
+           well as the variable of the class itself so if we were about to use
+           name = name; the compiler will be confused,it will assume that we are
+           assigning the value to the same argument variable,so this in this
+           sense is like telling a compiler that we are using name defined in the 
+           class or name which is a member variable of the class.
+       }
+
+       private:
+       string name;
+   }
+
+   use this to access member variables or function of a class.
+
 
     Default constructor and Constructor Overloading.
     ------------------------------------------------
-    comming soon
+A default constructor is a constructor with no argument,if you don't provide this
+constructor then the C++ compiler will gladly provide one for you.
+
+example:
+
+class A{
+
+    public:
+    A(){}//default constructor
+};
+
+Constructor Overloading
+-------------------------
+in C++,constructor overloading means to define more than one constructor
+with different arguments.C++ allows a program to define as many constructor
+as he/she wants provided that those constructors must have different arguments.
+
+example:
+
+class Dog{
+    public:
+    Dog(){
+        this is a default constructor
+    }
+
+    constructor with only one argument
+    Dog(string name){
+        this->name = name;
+    }
+
+    constructor with more than one argument
+    Dog(string name,int age): name(name),age(age){
+
+    }
+
+
+    private:
+    string name;
+    int age;
+};
+
+when creating an object of the class which has more than one constructor
+overloaded,the C++ compiler will determine the type of constructor to call
+depending on the value or argument given.For example from the above example
+
+Dog d1{}; this will call/invoke the constructor with no argument/defualt constructor
+
+Dog d2{"scooby"}; this will invoke the Dog constructor with one argument
+
+Dog d3{"scooby",5}; will invoke the third constructor with three arguments
+
+Note:
+
+if you define the constructor with arguments,then you might not want to
+define the default constructor as the constructor will be treated as defualt.Also
+when you create a class with constructor that takes an argument then the compiler will
+ not create a defualt constructor for you.
+
+ example:
+
+ class A{
+
+     public:
+     A(string name){
+         //this will be treated as defualt constructor
+     }
+ };
+
+ now:
+ A a{};//error: not arguments provided
+ A b{"scooby"};//ok one argument provided.
+
 
     Explicity keyword:
     -----------------
     comming soon
 
     Scope Resolution(::) Operator:
-    comming soon.
+    -----------------------------
+    The Scope resolution operator is used to access members of the class
+    that are implemented outside the class definition and all the static members
+    as well.This indicate that the function/method or variable belongs to the class.
+
+    example:
+
+    class D{
+        public:
+        D();
+        void say_hi();
+
+        private:
+        string name;
+    };
+
+    the above class consists of the constructor D() and one member function say_hi(),
+    both of this are defined but not implementd in the class.We ca implement these functions
+    as follows:
+
+    D::D(){
+        //here :: is a scope resolution denoting that first D is a name of class
+        and the second D is a name of construcor,so Second D defined and belongs to
+        the first D.
+    }
+
+void D::say_hi(){
+    //void is the function returning type,:: means this function is defined and
+    belongs to the class whose name is D.
+}
 
     Static Member functions:
     -----------------------
-    comming soon
+    All class member preceeded with static keywords are static members of the class.
+    these members belongs to the class and not the object of the class.
+    These means static members can be accessed with any object of the lass,but they 
+    only belongs to the class,Static members are accessed by using scope resolution 
+    operators but not dot nor this.
+
+    example:
+    class StaticDemo{
+
+        public:
+        StaticDemo(){
+
+        }
+        static void say_d(){
+            static member function
+        }
+
+        private:
+        static string name;//static member variable
+    }
     
+    to acess say_h:
+    StaticDemo::say_hi(); no need to create an instance of StaticDemo;
+
+    if you try:
+    StaticDemo d{}; then d.say_hi(); it works as static members can be accessed
+    by any class objects.
+
+
     Constant Objects:
     -----------------
     comming soon
@@ -267,7 +414,9 @@ int main(void){
 
     //scooby.s; error, s is private cannot be accessed from here
     //scooby.ps; error, ps is private cannot be accessed from here
-    Dog::say();
+    Dog s{};
+    s.say();//fine accessing static member of a class using class object.
+    
 
     return 0;
 }
